8162fdf54824ef244dadc84ddd8df11cc2c063cb
이후기 <lugan@naver.com>
2024-06-14 02:18:12 +0900

    * 64061. 크레인 인형뽑기 문제 생성

---
+++ src/프로그래머스/level1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.kt
@@ -0,0 +1,15 @@
+package 프로그래머스.level1.`64061`.` 크레인 인형뽑기 게임`
+
+fun main() {
+
+}
+
+/**
+ * @param board 크레인 게임화면의 격자 상태
+ * @param moves 크레인을 작동시킨 위치가 담긴 배열
+ * @return Int 크레인을 모두
+ * */
+fun solution(board: Array<IntArray>, moves: IntArray): Int {
+    var answer = 0
+    return answer
+}
