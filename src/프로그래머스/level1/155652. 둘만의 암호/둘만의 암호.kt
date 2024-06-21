06192ce9324e5c8b416b8e557e4471cadfa11937
이후기 <lugan@naver.com>
2024-06-21 14:07:29 +0900

    * 155652. 둘만의 암호 문제 생성

---
+++ src/프로그래머스/level1/155652. 둘만의 암호/둘만의 암호.kt
@@ -0,0 +1,15 @@
+package 프로그래머스.level1.`155652`.` 둘만의 암호`
+
+import junit.framework.TestCase.assertEquals
+
+fun main() {
+    solution("aukks", "wbqd", 5).run {
+        assertEquals("happy", this)
+    }
+}
+
+
+fun solution(s: String, skip: String, index: Int): String {
+    var answer: String = ""
+    return answer
+}
