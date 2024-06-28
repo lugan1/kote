48f0c6055f1531f374fe82dccbd97a6e958bcc03
이후기 <lugan@naver.com>
2024-06-28 13:36:27 +0900

    * 150370. 개인정보 수집 유효기간 제출

--- src/프로그래머스/level1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
+++ src/프로그래머스/level1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -1,5 +1,7 @@
 package 프로그래머스.level1.`150370`.` 개인정보 수집 유효기간`
 
+import java.time.*
+import java.time.format.*
 import kotlin.test.assertEquals
 
 fun main() {
@@ -12,6 +14,9 @@ fun main() {
     }
 }
 
+//DateTimeFormatter 는 import java.time.* 으로는 인식이 안된다. (주의)
+
+
 /**
  * @param today 오늘 날짜를 나타낸다 "YYYY.MM.DD"
  * @param terms 약관 종류, 유효기간 을 구분한 문자열 (1 ~ 20)
@@ -20,6 +25,29 @@ fun main() {
  * */
 fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
     var answer: IntArray = intArrayOf()
-    
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    val now = LocalDate.parse(today, formatter)
+
+    //todo: 문자열 별로 map 을 만든다. 예) A 6,
+    //todo: 개인정보에서 해당되는 약관(A) 가 오늘 날짜를 지났는지 확인한다.
+    //todo: 오늘 날짜를 지났으면 idnex 아니면 null
+
+    val termsMap: Map<String, Int> = terms.associate {
+        val list = it.split(" ")
+        list[0] to list[1].toInt()
+    }
+
+    answer = privacies.mapIndexed { i, value ->
+        val number = i+1
+        val privacy = value.split(" ")
+
+        val date = LocalDate.parse(privacy[0], formatter)
+        val term = termsMap[privacy[1]]!!
+        val expiredDate = date.plusMonths(term.toLong())
+
+        val expired = now.isAfter(expiredDate) || now.isEqual(expiredDate)
+        if(expired) number else null
+    }.filterNotNull().toIntArray()
+
     return answer
 }
